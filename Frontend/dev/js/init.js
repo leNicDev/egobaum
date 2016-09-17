177d53cf984cecd7a48072f678cd2cd34be7feb7
brajos <alex@brajos.de>
2016-09-17 18:53:46 +0200

    vue

--- Frontend/dev/js/init.js
+++ Frontend/dev/js/init.js
@@ -1,1 +1,11 @@
+window.onload = function () {
+
 console.log("test JS");
+
+new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue.js!'
+    }
+});
+}
--- index.js
+++ index.js
@@ -13,8 +13,15 @@ app.listen(8081, function () {
 function puts(error, stdout, stderr) { sys.puts(stdout) }
 
 app.get("/", function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end("INITIALES HALLO :-)");
+    res.sendfile('./Frontend/pub/index.html', {root: __dirname });
+});
+
+app.get("/css/egobaum.css", function(req, res) {
+    res.sendfile('./Frontend/pub/css/egobaum.css', {root: __dirname });
+});
+
+app.get("/js/egobaum.js", function(req, res) {
+    res.sendfile('./Frontend/pub/js/egobaum.js', {root: __dirname });
 });
 
 app.get("/init/:param", function(req, res) {

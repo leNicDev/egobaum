eec776748f4ef83abe4044534cf4f0a83021d565
tstoff <tim.stoffel@eventim.de>
2016-09-18 11:19:36 +0200

    some refactoring in building stuff and git ignore for new DB

--- serverScripts/utils.js
+++ serverScripts/utils.js
@@ -1,7 +1,8 @@
 console.log("create JS-Folder");
 var mkdirp = require('mkdirp');
-mkdirp('./Frontend/pub/js/', function(err) {
-    // TODO on error
+mkdirp('./Frontend/pub/js/', function(res) {
+    console.log("JS Folders created -------------------------------");
+}, function(err){
     console.log('Folder not created!!!');
+    console.warn(err);
 });
-console.log("JS Folders created -------------------------------");

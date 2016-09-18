49f4df8d50b07b82551c3404a414c09c5d916be4
brajos <alex@brajos.de>
2016-09-18 09:06:40 +0200

    zwischenstand. mix

--- Frontend/dev/js/init.js
+++ Frontend/dev/js/init.js
@@ -1,11 +1,25 @@
 window.onload = function () {
 
-console.log("test JS");
+    console.log("test JS");
 
-new Vue({
-    el: '#app',
-    data: {
-        message: 'Hello Vue.js!'
+    var jsonData = {};
+
+    function getInitJSON(egoID) {
+        jQuery.getJSON("/mock", function(result){
+            console.log("JSON respond: ");
+            console.log(JSON.stringify(result));
+            jsonData = result;
+
+            new Vue({
+                el: '#app',
+                data: {
+                    message: jsonData
+                }
+            });
+        });
     }
-});
+
+
+
+    getInitJSON(10);
 }
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # egobaum 1.0
 
 DAS Tool im Internet für tolle Bäume ;-)
+
+TODO:
+- Sourcemap
+- watch task
+- 

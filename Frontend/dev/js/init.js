1b6bbd47979c1d1e4e65d106a8283230178f68a2
brajos <alex@brajos.de>
2016-09-22 10:24:43 +0200

    other start point. small tree

--- Frontend/dev/js/init.js
+++ Frontend/dev/js/init.js
@@ -1,8 +1,8 @@
 window.onload = function () {
     var self = this;
     //self.egoID = 'fdd0e7bc69efb8736e4555873900404f';
-    //self.egoID = '0b25d53ddd83594de9a839892c003cfa';  // Familie Tisch
-    self.egoID = '0b25d53ddd83594de9a839892c00801a';  // CHAOS
+    self.egoID = '0b25d53ddd83594de9a839892c003cfa';  // Familie Tisch
+    //self.egoID = '0b25d53ddd83594de9a839892c00801a';  // CHAOS
     self.globalJSON = {};
     self.nextID = '';
     self.degree = 1;
